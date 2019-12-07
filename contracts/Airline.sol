a4074b8255ef7553a3e978da8277bfcda166dfd7
MartinOber16 <martin.obermeier@gmail.com>
2019-12-07 18:52:23 -0300

    Airline.sol

--- contracts/Airline.sol
+++ contracts/Airline.sol
@@ -2,42 +2,47 @@ pragma solidity ^0.4.24;
 
 contract Airline {
     address public owner;
+    // Cliente
     struct Customer {
         uint loyaltyPoints;
-        uint totalFlights;
+        uint totalFlights; // numero de vuelos totales que ha comprado
     }
 
+    // Vuelo
     struct Flight {
         string name;
         uint price;
     }
 
+    // Listado de vuelos
     Flight[] public flights;
 
-    mapping(address => Customer) public customers;
-    mapping(address => Flight[]) public customerFlights;
-    mapping(address => uint) public customerTotalFlights;
+    mapping(address => Customer) public customers; // Relaciona la dirección con el Cliente
+    mapping(address => Flight[]) public customerFlights; // Relaciona los vuelos de un Cliente por su dirección
+    mapping(address => uint) public customerTotalFlights; // Total de los vuelos que ha comprado un cliente
 
-    event FlightPurchased(address indexed customer, uint price);
+    event FlightPurchased(address indexed customer, uint price); // Evento para cada compra
 
-    constructor() {
+    constructor() public {
         owner = msg.sender;
+        // Vuelos iniciales
         flights.push(Flight('tokio',4 ether));
         flights.push(Flight('Germany',1 ether));
         flights.push(Flight('Madrid',2 ether));
     }
 
+    // Funcion para comprar el vuelo
     function buyFlight(uint flightIndex) public payable {
-        Flight memory flight = flights[flightIndex];
-        require(msg.value == flight.price);
+        Flight memory flight = flights[flightIndex]; // Identificar el vuelo que el cliente va a comprar
+        require(msg.value == flight.price); // Requerir la cantidad de ethers del precio del vuelo
 
-        Customer storage customer = customers[msg.sender];
+        Customer storage customer = customers[msg.sender]; // Identificar el Cliente
         customer.loyaltyPoints += 5;
-        customer.totalFlights ++;
+        customer.totalFlights += 1;
         customerFlights[msg.sender].push(flight);
         customerTotalFlights[msg.sender] ++;
 
-        FlightPurchased(msg.sender, flight.price);
+        emit FlightPurchased(msg.sender, flight.price);
     }
 
 }
