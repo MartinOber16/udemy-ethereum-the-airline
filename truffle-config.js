093876ae8e5d992ca2bbd3612654c98a43e998db
Martin Ober <martinober@MacBook-Air-de-Martin.local>
2019-12-12 00:17:48 -0300

    20191211

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = 'scrap fence music urban marine wheel diamond wall wash security candy retreat';
 module.exports = {
   networks: {
     development: {      
@@ -5,6 +7,10 @@ module.exports = {
       port: 7545,
       network_id: '*',
       gas: 5000000
+    },
+    rinkeby: {      
+      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/LtjZFi6C8XO0qkthtI7p"), // api de infura
+      network_id: '4'
     }
   }
 }
